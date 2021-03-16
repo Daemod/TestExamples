8fbcb9c2035123cd3eb625dc43c5f749e30d53ff
tayker <v.severyukhin@gateline.ru>
2021-03-16 18:30:05 +0300

    Комменты.

--- src/main/java/ru/sample/examples/AppStarter.java
+++ src/main/java/ru/sample/examples/AppStarter.java
@@ -11,14 +11,19 @@ import java.io.InputStreamReader;
 
 public class AppStarter {
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Animal cat = new Cat(reader);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //Создаем экземляр для чтения из консоли
+
+        Animal cat = new Cat(reader); //передаем ссылку reader в cat
         System.out.println(cat.getName());
-        Animal dog = new Dog(reader);
-        Animal hamster = new Hamster(reader);
+
+        Animal dog = new Dog(reader); //передаем ссылку reader в dog
+        System.out.println(dog.getName());
+
+        Animal hamster = new Hamster(reader);//передаем ссылку reader в hamster
+        System.out.println(hamster.getName());
 
         try {
-            reader.close();
+            reader.close(); //Закрываем чтение из консоли, т.к мы передавли ссылку на reader то BufferedReader закроется во всех Animals.
         } catch (IOException e) {
             e.printStackTrace();
         }
